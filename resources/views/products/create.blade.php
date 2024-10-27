eb6ec13818953ddfe64971087dd771f4fda62a82
Veerapat <veerapat.bunk@gmail.com>
2024-10-27 10:50:28 +0700

    update create page
    - Change css
    - Setup input type-parameter

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,33 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file if needed -->
+
     <title>Document</title>
 </head>
 <body>
-<div class="container">
+    <div class="container">
         <h1>Create Product</h1>
         <form method="post" action="{{route('product.store')}}">
         @csrf 
         @method('post')
-        <div>
-            <label>Name</label>
-            <input type="text" name="name" placeholder="Name" />
-        </div>
-        <div>
-            <label>Qty</label>
-            <input type="text" name="qty" placeholder="Qty" />
-        </div>
-        <div>
-            <label>Price</label>
-            <input type="text" name="price" placeholder="Price" />
-        </div>
-        <div>
-            <label>Description</label>
-            <input type="text" name="description" placeholder="Description" />
-        </div>
-        <div>
-            <input type="submit" value="Save a New Product" />
-        </div>
+        <div class="form-group">
+                <label for="product-name">Product Name:</label>
+                <input type="text" id="name" name="name" required>
+                
+            </div>
+
+            <div class="form-group">
+                <label for="product-description">Description:</label>
+                <textarea id="description" name="description" rows="4" required></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="product-price">Price:</label>
+                <input type="number" id="price" name="price" placeholder="0.00" step="0.01" required>
+            </div>
+
+            <div class="form-group">
+                <label for="product-quantity">Quantity:</label>
+                <input type="number" id="qty" name="qty" placeholder="0" required>
+            </div>
+
+
+            <button type="submit">Create Product</button>
     </form>
     </div>
     <style>
@@ -42,9 +48,10 @@
         .container {
             width: 50%;
             margin: 50px auto;
-            padding: 20px;
+            padding: 50px;
             background: #fff;
             border-radius: 5px;
+            
             box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
         }
         h1 {
